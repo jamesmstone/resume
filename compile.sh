2bc3e1893d817335da6162e45a05e5aa6d7eb084
James Stone <jamesmstone@users.noreply.github.com>
2017-03-26 11:46:20 +1100

    Add analytics

--- compile.sh
+++ compile.sh
@@ -8,4 +8,5 @@ jsonresume ()
 jsonresume export flat.html
 jsonresume export -t elegant index.html
 sudo tee -a index.html < fork.html
+sudo tee -a index.html < analytics.html
 jsonresume export -f pdf resume.pdf
